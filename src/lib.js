8797a5064f1f6e0033ddade010fd8c86e229483c
Your Name <arlofilley@gmail.com>
2026-01-21 06:19:56 +0000

    Readability refactor

--- src/lib.js
+++ src/lib.js
@@ -1,16 +1,20 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+const readline = require('node:readline/promises');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function ask(q) {
+    let a = await rl.question(q);
+    return a.toLowerCase();
 }
 
-function count(word, char) {
-    let count = 0;
-    for (let letter of word) {
-        if (letter === char) { count++ }
-    }
-
-    return count
-}
-
+/**
+ * Calculates the positional frequencies of letters based on a list of words
+ * @param {Array<Array<string>>} words 
+ * @returns 
+ */
 function calculatePosFreq(words) {
     let positional_frequencies = Array.from({length: 5}, () => ({}));
 
@@ -24,4 +28,67 @@ function calculatePosFreq(words) {
     return positional_frequencies;
 }
 
-module.exports = { getRandomInt, count, calculatePosFreq }
+/**
+ * Counts the number of a given character in a word
+ * @param {*} word 
+ * @param {*} char 
+ * @returns 
+ */
+function count(word, character) {
+    let count = 0;
+    for (let letter of word) {
+        if (letter === character) { count++ }
+    }
+
+    return count
+}
+
+
+/**
+ * Scores a word based on positional frequencies of letters
+ * @param {string} word 
+ * @param {number} guesses 
+ * @param {Array<[char,number]>} positional_frequencies 
+ * @returns 
+ */
+function score(word, guesses, positional_frequencies) {
+    let score = 0;
+    const seen = new Set();
+    for (const i in word) {
+        let c = word[i];
+        if (!seen.has(c)) {
+            score += positional_frequencies[i][c];
+            seen.add(c);
+        } else {
+            score += positional_frequencies[i][c] / (6 - guesses);
+        }
+    }
+    return score;
+}
+
+/**
+ * Checks if a word meets conditions with the min, max, fixed, and banned position of characters
+ * @param {string} word 
+ * @param {Array<[char, object]>} conditions 
+ */
+function meetsConditions(word, letter_conditions) {
+    for (const l of letter_conditions) {
+        const letter = l[0];
+        const conditions = l[1];
+        const c = count(word, letter);
+
+        if (c > conditions.max) { return false; }
+        if (c < conditions.min) { return false; }
+        for (const i of conditions.fixed_positions) {
+            if (word[i] !== letter) { return false; }
+        }
+        for (const i of conditions.banned_positions) {
+            if (word[i] === letter) { return false; }
+        }
+    }
+
+    return true;
+}
+
+
+module.exports = { ask, calculatePosFreq, count, meetsConditions, score }
--- src/main.js
+++ src/main.js
@@ -1,25 +1,13 @@
 let words = require('../data/filtered_words.json');
-let starting_pos_freq = require('../data/positional_frequencies.json')
+let global_pf = require('../data/positional_frequencies.json')
+const { ask, calculatePosFreq, count, meetsConditions, score } = require('./lib.js')
 
-const { calculatePosFreq, count } = require('./lib.js')
-const readline = require('node:readline/promises');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let word_numbers = [words.length]
+let word_numbers = []
 
 async function main() {
-    let pos_freq = starting_pos_freq;
-    let got_word = 'f'
+    let fw = words;
+    let pf = global_pf;
     let guesses = 0;
-    // Conditions 'the word' has to fulfill
-    // min              - the word contains at least a certain number of a letter
-    // max              - the word contains no more than a certain number of a letter
-    // fixed_positions  - a letter shows up at a certain position of the word
-    // banned_positions - a letter doesn't show up at a certain position of the word
 
     /** @type Map<char, object> */
     let conditions = new Map();
@@ -33,20 +21,50 @@ async function main() {
         })
     }
 
-    console.log(`Guess 1: CLOTH`)
-    while (got_word.toLowerCase() !== 't') {
-        // Guess
-        // Input
-        // Constraints
-        // Filter
-        // Score
-        guesses += 1;
-        const filtered_words = [];
-        const green = (await rl.question("Green letters - use '.' for blanks: ")).toLowerCase();
-        const yellow = (await rl.question("Yellow letters - use '.' for blanks: ")).toLowerCase();
-        const grey = (await rl.question("Grey letters - use '.' for blanks: ")).toLowerCase();
-
+    while (true) {
+        // Strategize & Score
+        let best_score = 0;
+        let best_guess = 0;
+        let best_guesses = [];
+        if (fw.length < 500) {
+            pf = calculatePosFreq(fw);
+            for (const w of fw) {
+                let s = score(w, guesses, pf)
+                if (s > best_score) {
+                    best_score = s;
+                    best_guess = w;
+                    best_guesses.push(best_guess);
+                }
+            }
+        } else {
+            for (const w of words) {
+                let s = score(w, guesses, pf)
+                if (s > best_score) {
+                    best_score = s;
+                    best_guess = w;
+                    best_guesses.push(best_guess);
+                }
+            }
+        }
         
+        // Guess
+        console.log(`Possible Words Left: ${fw.length}\n`);
+        word_numbers.push(fw.length);
+        if (fw.length < 50) {
+            console.log(`Best Guess ${guesses+1}: ${best_guess}`);
+            console.log(`Other Guesses ${guesses+1}: ${fw}`);
+        } else {
+            console.log(`Best Guess ${guesses+1}: ${best_guesses}`);
+        }
+        guesses += 1;
+        
+        // Input
+        
+        const green  = await ask("Green Letters. - use '.' for blanks: ");
+        const yellow = await ask("Yellow letters - use '.' for blanks: ");
+        const grey   = await ask("Grey letters   - use '.' for blanks: ");
+
+        // Constraints
         // Set Min Using Green & Yellow Input
         for (const letter of green + yellow) {
             if (letter !== '.') {
@@ -80,89 +98,29 @@ async function main() {
             }
         }
         
+        // Filter
+        const filtered_words = [];
         const cond = [...conditions].map((e) => e)
-        for (let word of words) {
-            if (fits_conditions(word, cond)) {
+        for (let word of fw) {
+            if (meetsConditions(word, cond)) {
                 filtered_words.push(word)
             }
         }
 
-        if (filtered_words.length === 0) {
+        fw = filtered_words;
+
+        if (fw.length === 0) {
             console.log("ERROR: No Correct Word Could Be Found");
-            return;
+            process.exit();
         } 
         
-        if (filtered_words.length === 1) {
-            console.log(`Words: ${filtered_words.length}`);
+        if (fw.length === 1) {
             console.log(`Solution: ${filtered_words}`);
-            console.log(`Guess ${guesses+1} - Yippeee!`);
+            console.log(`Guess ${guesses} - Yippeee!`);
             console.log(`Word Numbers By Guess: ${word_numbers}`);
-            rl.close();
-            return;
+            process.exit();
         }
-
-        let best_score = 0;
-        let best_guess = "";
-
-        if (filtered_words.length < 500) {
-            pos_freq = calculatePosFreq(filtered_words);
-        }
-
-        for (const w of words) {
-            let s = score(w, guesses, pos_freq)
-            if (s > best_score) {
-                best_score = s;
-                best_guess = w;
-            }
-        }
-
-        if (guesses === 1) {
-            best_guess = "bares"
-        }
-            
-        console.log(`words: ${filtered_words}`);
-        console.log(`Possible Words Left: ${filtered_words.length}\n`)
-        console.log(`Guess ${guesses+1}: ${best_guess.toUpperCase()}`);
-        word_numbers.push(filtered_words.length)
     }
 }
 
 main();
-
-/**
- * @param {string} word 
- * @param {Array<[char, object]>} conditions 
- */
-function fits_conditions(word, letter_conditions) {
-    for (const l of letter_conditions) {
-        const letter = l[0];
-        const conditions = l[1];
-        const c = count(word, letter);
-
-        if (c > conditions.max) { return false; }
-        if (c < conditions.min) { return false; }
-        for (const i of conditions.fixed_positions) {
-            if (word[i] !== letter) { return false; }
-        }
-        for (const i of conditions.banned_positions) {
-            if (word[i] === letter) { return false; }
-        }
-    }
-
-    return true;
-}
-
-function score(word, guesses, positional_frequencies) {
-    let score = 0;
-    const seen = new Set();
-    for (const i in word) {
-        let c = word[i];
-        if (!seen.has(c)) {
-            score += positional_frequencies[i][c];
-            seen.add(c);
-        } else {
-            score += positional_frequencies[i][c] / (6 - guesses);
-        }
-    }
-    return score;
-}
